bcf17e8d8360ac3752e4c0b6379e277ffa924bb5
Lynn <145916700+Linhlinhlinhlinhlinh@users.noreply.github.com>
2024-03-31 15:08:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 st.set_page_config(page_title="BFF", page_icon='💖', layout='wide')
 st.title("Hay nhap ten nhung bff cua ban")
 name = st.text_input('Hay nhap ten bff cua ban')
-gender = st.text_input('Hay nhap gioi tinh cua ban')
-age = st.text_input('Hay nhap tuoi cua ban')
+gender = st.text_input('Hay nhap gioi tinh bff cua ban')
+age = st.text_input('Hay nhap tuoi bff cua ban')
 if name != '' and gender != '' and age != '':
   s = st.button('NEXT')
 else:
