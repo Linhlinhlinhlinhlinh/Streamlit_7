1221be00c451680b2eea77b49969e5301f006026
Lynn <145916700+Linhlinhlinhlinhlinh@users.noreply.github.com>
2024-03-31 15:05:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import streamlit as st
 st.set_page_config(page_title="BFF", page_icon='💖', layout='wide')
 st.title("Hay nhap ten nhung bff cua ban")
 name = st.text_input('Hay nhap ten bff cua ban')
-gender = st.selectbox('Hay nhap gioi tinh cua ban', ['Nam', 'Nu'])
+gender = st.text_input('Hay nhap gioi tinh cua ban')
+age = st.text_input('Hay nhap tuoi cua ban')
+if name != '' and gender != '' and age != '':
+  s = st.button('NEXT')
+else:
+  st.warning('Vui long nhap day du thong tin')
+if 's' in local:
+  if s :
+    st.clear()
